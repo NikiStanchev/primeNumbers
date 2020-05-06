d09b397ed1e32f072d07d9b25a70d463b498d3aa
NikiStanchev <nikolay_stanchev@mail.com>
2020-05-06 14:32:06 +0300

    Add unit tests

--- src/test/java/com/nikolay/stanchev/prime/PrimeNumbersApplicationTests.java
+++ src/test/java/com/nikolay/stanchev/prime/PrimeNumbersApplicationTests.java
@@ -1,13 +1,66 @@
 package com.nikolay.stanchev.prime;
 
+import com.nikolay.stanchev.prime.models.PrimeNumber;
+import com.nikolay.stanchev.prime.services.impl.PrimeNumberServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = PrimeNumbersApplication.class)
+@WebAppConfiguration
 class PrimeNumbersApplicationTests {
 
+    @Autowired
+    PrimeNumberServiceImpl primeNumberService;
+
     @Test
-    void contextLoads() {
+    void isPrimeNumber_19_true() {
+        Long number = 19L;
+
+        PrimeNumber primeNumber = new PrimeNumber(number, "Is a prime number!");
+        PrimeNumber result = primeNumberService.isPrimeNumber(number);
+
+        assertEquals(primeNumber.getNumber(), result.getNumber());
+        assertEquals(primeNumber.getMessage(), result.getMessage());
     }
 
+    @Test
+    void isPrimeNumber_18_false() {
+        Long number = 18L;
+
+        PrimeNumber primeNumber = new PrimeNumber(number, "Is not a prime number!");
+        PrimeNumber result = primeNumberService.isPrimeNumber(number);
+
+        assertEquals(primeNumber.getNumber(), result.getNumber());
+        assertEquals(primeNumber.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void nextPrimeNumber_18_19() {
+        Long number = 18L;
+
+        PrimeNumber primeNumber = new PrimeNumber(19L, String.format("Is the next primer number after %d.", number));
+        PrimeNumber result = primeNumberService.nextPrimeNumber(number);
+
+        assertEquals(primeNumber.getNumber(), result.getNumber());
+        assertEquals(primeNumber.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void nextPrimeNumber_minus2_2() {
+        Long number = -2L;
+
+        PrimeNumber primeNumber = new PrimeNumber(2L, String.format("Is the next primer number after %d.", number));
+        PrimeNumber result = primeNumberService.nextPrimeNumber(number);
+
+        assertEquals(primeNumber.getNumber(), result.getNumber());
+        assertEquals(primeNumber.getMessage(), result.getMessage());
+    }
 }
